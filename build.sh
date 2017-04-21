1b2d39b4a188c6da18e8e70e3f9d721df515c713
Ben Roubicek <ben.roubicek@freestar.io>
2017-04-21 14:30:47 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 tag=${1:-latest}
-registryUrl="us.gcr.io/freestar-157323/slackin-app"
+registryUrl="us.gcr.io/freestar-157323/slackin"
 
 echo "Building $tag"
 
 # Build the main application Dockerfile
-docker build -t "slackin-app:$tag" .
-docker tag "slackin-app:$tag" "$registryUrl:$tag"
+docker build -t "slackin:$tag" .
+docker tag "slackin:$tag" "$registryUrl:$tag"
 
 # Push to gcloud container registry
 gcloud docker -- push "$registryUrl:$tag"
